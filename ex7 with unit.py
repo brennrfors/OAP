a6027aca16ee1e33323f9358c76a1a10e3afa18d
brennrfors <39821752+brennrfors@users.noreply.github.com>
2020-05-31 10:56:13 +0300

    Updated and cleaned

--- ex7 with unit.py
+++ ex7 with unit.py
@@ -1,10 +1,8 @@
 import numpy as np 
 import unittest
 
-
-
-
-class TestStringMethods(unittest.TestCase):
+#Unit test
+class Test_Methods(unittest.TestCase):
 
 	def test_sum(self):
 		self.assertEqual(sum_ar(1,2), 3)
@@ -12,27 +10,29 @@ class TestStringMethods(unittest.TestCase):
 	def test_sub(self):
 		self.assertEqual(sub(10, 7), 3)
 
-
+#generrating numbers from 0 to 255 in 3d array
 def generate():
 	a = np.random.randint(255, size=(3,3,3))
 	b = np.random.randint(255, size=(3,3,3)) 
 
 	return a, b
-
+#summing array
 def sum_ar(a, b):
 	print("sum of a and b")
 	return np.sum([a,b])
-
+#substraction of arrays
 def sub(a ,b):
 	print("a - b: \n", np.sum([a, b*-1]))
 	print("\nb - a: \n", np.sum([b, a*-1]))
 	return int(np.sum([a, b*-1]))
 
+#scaling(multipling every element by scalar)
 def scalar(a, b, scal):
 	print("Scalar of a and b: \n")
 	print("A * scal = \n", a*int(scal))
 	print("B * scal = \n", b*int(scal))
-
+	
+#sum of elements of array/bigger sum
 def sum_of_and_big(a, b):
 	print("Sum of mass:\na = ", np.sum(a), "\nb = ", np.sum(b))
 	if np.sum(a) > np.sum(b):
@@ -43,28 +43,10 @@ def sum_of_and_big(a, b):
 		print("Sums are equal")
 
 
-data = generate()
-a = data[0]
-b = data[1]
-
-print("Enter number to choose: 1 - sum, 2 - higher, 3 - scalar, 4 - sum of mass")
-en = input()
-
-if en == 1:
-	sum_ar(a, b)
-elif en == 2:
-	sub(a,b)
-elif en == 3:
-	print("choose scalar :")
-	sc = input()
-	scalar(a, b, sc)
-elif en == 4:
-	sum_of_and_big(a,b)
-
-
 if __name__ == '__main__':
+	#running unittest
 	unittest.main()
-
+	#if ok -> run program
 
 
 	data = generate()
